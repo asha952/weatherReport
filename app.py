a4edea643b539eaeb16cf5cc4021151696cb5ec3
asha952 <ashabsiddiqui952@gmail.com>
2019-12-12 00:32:30 -0800

    fixed app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, app
+from flask import Flask, render_template, request
 import requests
+
+
+app = Flask(__name__)
 
 
 @app.route('/weather')
@@ -9,19 +12,15 @@ def weather():
 
 @app.route('/weather_results')
 def weather_results_page():
-    weather_url = "https://api.openweathermap.org/data/2.5/weather"
     users_city = request.args.get('city')
+
     params = {
         'q': users_city,
-        'appid': 'a6e42a57571246296ace899c0fc83ba7'
+        'appid':"9f87d4a1669aa1f928d4ce146001bdec"
+
     }
-    response = requests.get(weather_url, params=params)
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather', params=params)
+
     response_json = response.json()
-    city = response_json['name']
-    temp = response_json['main']['temp']
-
-    return render_template('weather_results.html', city=city, temp=temp)
-
-
-if __name__ == '__main__':
-    app.run()
+    response_temp = response_json.get('main').get('temp')
+    return render_template('weather_results.html', results=response_temp)
